7307b3f50f1c2cc461f0cecef281def17e01e954
ikorb0197 <ikorb0197@floridapoly.edu>
2025-12-14 22:25:42 -0500

    Add computer choice (random)

--- game.py
+++ game.py
@@ -1,1 +1,5 @@
+from random import choice
 print("Welcome to Rock-Paper-Scissors!")
+choices = ["rock", "paper", "scissors"]
+computer_choice = choice(choices)
+print(f"Computer chose: {computer_choice}")
